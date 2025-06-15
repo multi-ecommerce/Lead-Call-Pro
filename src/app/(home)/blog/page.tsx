b049984411a812a3d10a1c37ffa454649e1d50d4
Kashif-Awann <awank5692@gmail.com>
2025-06-15 15:41:56 +0500

    About & Blog page

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -1,7 +1,7 @@
 export default function Blog() {
   return (
     <div>
-      <div className="bg-[url('/images/page-header-bg.webp')] bg-cover bg-center bg-no-repeat h-[32rem] w-full flex items-center px-8 lg:px-20 relative -top-12">
+      <div className="bg-[url('/images/page-header-bg.webp')] bg-cover bg-center bg-no-repeat h-[32rem] w-full flex items-center px-8 lg:px-20 relative -top-16">
         <div className="text-white font-mono mt-24">
           <p className="text-lg font-semibold pl-1">BLOG POST</p>
           <p className="text-5xl font-bold">Latest News</p>
